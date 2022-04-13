52a3c154846f6ce26a9b14d880ed0df10dfd112c
DLNATE <artemsokolov1411@gmail.com>
2022-04-14 00:49:03 +0300

    v1.0

--- my_app.py
+++ my_app.py
@@ -1,1 +1,40 @@
 # напиши здесь код основного приложения и первого экрана
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QHBoxLayout,
+    QMessageBox,
+    QLineEdit,
+    QPushButton
+)
+
+mainApp = QApplication([])
+
+class MainWin(QWidget):
+    def clicked(self):
+        print('АААА')
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.mainLayout = QVBoxLayout()
+        self.helloText = QLabel(txt_hello)
+        self.startBtn = QPushButton(txt_next)
+        self.mainLayout.addWidget(self.helloText)
+        self.mainLayout.addWidget(self.startBtn)
+        self.setLayout(self.mainLayout)
+    def connects(self):
+        self.startBtn.clicked.connect(self.clicked)
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+startwin = MainWin()
+mainApp.exec_()
