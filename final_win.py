1ce9df6444b85e9eb3a88bf5ce4789429183ba3d
DLNATE <artemsokolov1411>
2022-04-14 18:12:22 +0300

    v.0.1

--- final_win.py
+++ final_win.py
@@ -1,1 +1,13 @@
 # напиши здесь код третьего экрана приложения
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QHBoxLayout,
+    QMessageBox,
+    QLineEdit,
+    QPushButton
+)
