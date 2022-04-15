97ffe7763ad0684490f923135727bd2e16613853
DLNATE <artemsokolov1411>
2022-04-15 16:28:02 +0300

    v.0.2

--- final_win.py
+++ final_win.py
@@ -11,3 +11,21 @@ from PyQt5.QtWidgets import (
     QLineEdit,
     QPushButton
 )
+class FinalWin(QWidget):
+    def clicked(self):
+        pass
+    def set_appear(self):
+        self.setWindowTitle(txt_finalwin)
+        self.setFixedSize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        pass
+    def connects(self):
+        pass
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+        
