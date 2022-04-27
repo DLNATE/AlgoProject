6ef86884c2ec56011dae9cc2a73af889031ba85a
DLNATE <artemsokolov1411@gmail.com>
2022-04-27 10:43:00 -0700

    Revert "v.1.3.3"
    This reverts commit fa385c9ffe73b2f1317370be02f0eea9de813ba1.

--- my_app.py
+++ my_app.py
@@ -2,7 +2,6 @@
 from instr import *
 import json
 import os.path
-import os
 from second_win import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
@@ -83,7 +82,6 @@ def main():
             self.initUI()
             self.connects()
             self.show()
-    os.mkdir('src/')
     data = {
         'user.name' : '',
         'user.age' : '',
@@ -123,6 +121,8 @@ def main():
             self.mainLayout.addWidget(self.userNameLine, alignment= Qt.AlignCenter)
             self.mainLayout.addWidget(self.userAgeLine, alignment= Qt.AlignCenter)
             self.mainLayout.addWidget(self.profilecreateBtn, alignment= Qt.AlignCenter)
+            
+
             self.profilecreateBtn.clicked.connect(self.saveProfile)
         def newProfileInit(self):
             self.profileInit(self.userData)
@@ -186,8 +186,7 @@ def main():
                     self.radioBtnGroup.addButton(self.thirdProfileBtn)
                 self.mainLayout.addLayout(self.profilesLayout)
         def runMainWindow(self):
-            self.hide()
-            self.mw = MainWin(self.userData)
+            mw = MainWin(self.userData)
         def connects(self):
             self.newProfileBtn.clicked.connect(self.creatProfile)   
             self.goGuest.clicked.connect(self.runMainWindow)
